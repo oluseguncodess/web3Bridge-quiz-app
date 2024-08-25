804e6b18e2223bb6c34ea0fe17d2de98e4390755
olusegun <olusegunbamgbelu755@gmail.com>
2024-08-25 04:38:13 +0100

    configure tailwind.js file

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,10 @@ module.exports = {
     extend: {
       fontFamily: {
         'sans': "'Poppins', sans-serif" 
-      }
+      },
+      boxShadow: {
+        'bt': '0px 3px 5px 1px rgba(0, 0, 0, 0.1)'
+      },
     },
   },
   plugins: [],
