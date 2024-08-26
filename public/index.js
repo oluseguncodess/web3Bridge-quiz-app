60cd5919dbe9cfec65824925380a1496e39a198a
olusegun <olusegunbamgbelu755@gmail.com>
2024-08-26 18:11:09 +0100

    buttons active
    almost ran mad here cause of an inherited css styles that wasn't making the buttons clickable

--- public/index.js
+++ public/index.js
@@ -3,3 +3,32 @@ const start = document.querySelector('.start');
 const rules = document.querySelector('.rules');
 const questions = document.querySelector('.questions');
 const result = document.querySelector('.result');
+
+const startBtn = document.querySelector('.start-btn');
+const exitBtn = document.querySelector('.exitBtn');
+
+function rulesOpen() {
+    rules.classList.remove("opacity-0");
+    rules.classList.add("active");
+}
+
+function rulesClose() {
+    rules.classList.add("opacity-0");
+    rules.classList.remove("active");
+}
+
+function startQuestions () {
+    rulesClose();
+    questions.classList.remove("opacity-0");
+    questions.classList.add("active");
+
+}
+    
+//start quiz button    
+start.addEventListener("click", rulesOpen);
+
+//exit quiz button
+exitBtn.addEventListener("click", rulesClose);
+
+//continue quiz button
+startBtn.addEventListener("click", startQuestions)
