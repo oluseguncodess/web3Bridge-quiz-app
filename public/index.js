e77057cc285b8f37389fc4b69b7c8ce0789c828c
olusegun <olusegunbamgbelu755@gmail.com>
2024-08-26 16:02:08 +0100

    use the dom to get stuff

---
+++ public/index.js
@@ -0,0 +1,5 @@
+//get all the buttons and sections
+const start = document.querySelector('.start');
+const rules = document.querySelector('.rules');
+const questions = document.querySelector('.questions');
+const result = document.querySelector('.result');
